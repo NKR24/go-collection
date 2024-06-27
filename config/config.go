e627e32965f1d58df1fb950c86984b44813b38fc
Nikita Khvorostin <khvorostinn@gmail.com>
2024-06-28 01:21:18 +0300

    Created config file

---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+}
+
+func GetString(key string) string {
+	return viper.GetString(key)
+}
+
+func GetInt(key string) int {
+	return viper.GetInt(key)
+}
