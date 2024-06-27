12d1c0bbdd3cc19ea9cb1268e897bf13a8decf57
Nikita Khvorostin <khvorostinn@gmail.com>
2024-06-28 01:25:23 +0300

    Created init db file with config

---
+++ db/db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"api/config"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Init() {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Europe/Moscow",
+		config.GetString("db.host"),
+		config.GetString("db.user"),
+		config.GetString("db.password"),
+		config.GetString("db.name"),
+		config.GetInt("db.port"))
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
